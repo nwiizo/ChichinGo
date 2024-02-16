b0f887163cd6e7d151fe0b576ccf3468053309a7
nwiizo <syu.m.5151@gmail.com>
2024-02-16 19:59:33 +0900

    init

--- cmd/execute.go
+++ cmd/execute.go
@@ -4,70 +4,99 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"net"
 	"os"
-	"os/exec"
-	"strings"
-	"sync"
+	"path/filepath"
 )
 
-// executeCmd represents the execute command
+// executeCmd はコマンド実行を担当するcobraコマンドです。
 var executeCmd = &cobra.Command{
 	Use:   "execute",
 	Short: "Execute commands on specified hosts",
 	Long: `This command executes specified commands on a list of hosts defined in a configuration file.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Load config
-		viper.SetConfigName("config") // name of config file (without extension)
-		viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
-		viper.AddConfigPath(".")      // optionally look for config in the working directory
-		err := viper.ReadInConfig()   // Find and read the config file
-		if err != nil {               // Handle errors reading the config file
-			fmt.Printf("Fatal error config file: %s \n", err)
-			os.Exit(1)
-		}
-
-		hosts := viper.GetStringSlice("hosts")
-		restrictedCommands := viper.GetStringSlice("restricted_commands")
-
-		for _, cmd := range args {
-			for _, restricted := range restrictedCommands {
-				if strings.Contains(cmd, restricted) {
-					fmt.Printf("Restricted command detected: %s\n", cmd)
-					os.Exit(1)
-				}
-			}
-		}
-
-		var wg sync.WaitGroup
-		for _, host := range hosts {
-			wg.Add(1)
-			go func(host string) {
-				defer wg.Done()
-
-				fmt.Printf("Executing on host: %s\n", host)
-				sshCommand := fmt.Sprintf("ssh %s %s", host, strings.Join(args, " "))
-				cmd := exec.Command("bash", "-c", sshCommand)
-				output, err := cmd.CombinedOutput()
-				if err != nil {
-					fmt.Printf("Error executing command on host %s: %v\n", host, err)
-				}
-				fmt.Println(string(output))
-			}(host)
-		}
-		wg.Wait()
-	},
+	Run: execute,
 }
 
 func init() {
 	rootCmd.AddCommand(executeCmd)
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// executeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// executeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func execute(cmd *cobra.Command, args []string) {
+	config, err := loadConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, host := range config.Hosts {
+		executeOnHost(host, args)
+	}
+}
+
+func loadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".") // または他の設定ファイルのパス
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func executeOnHost(host HostConfig, commands []string) {
+	// 公開鍵認証の設定
+	key, err := ioutil.ReadFile(filepath.Join(os.Getenv("HOME"), ".ssh", "id_rsa"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to read private key: %v\n", err)
+		return
+	}
+
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse private key: %v\n", err)
+		return
+	}
+
+	config := &ssh.ClientConfig{
+		User: host.User,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // 実際の使用ではより安全な方法を選択してください
+	}
+
+	// SSH接続
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", host.Name, host.Port), config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to dial: %v\n", err)
+		return
+	}
+	defer client.Close()
+
+	// セッションの作成
+	session, err := client.NewSession()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create session: %v\n", err)
+		return
+	}
+	defer session.Close()
+
+	// コマンドの実行
+	cmd := strings.Join(commands, " ")
+	output, err := session.CombinedOutput(cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to execute command: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Output from %s: %s\n", host.Name, output)
+}
